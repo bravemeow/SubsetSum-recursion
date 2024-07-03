8f1d5818a677236fd2dd2c1f64c7ab6fc7875b27
Henry <bravemeow5@gmail.com>
2024-07-03 01:34:29 -0400

    SubsetSum Project completed.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # SubsetSum-recursion
 
-## Objective
+## @Objective
 
 ### Input
 input array : {1,2,3,4,5,6,7}\
@@ -9,3 +9,7 @@ target sum : 9
 ### Output
 subsets : {1,2,6}, {1,3,5}, {2,3,4}, {2,7}, {3,6}, {4,5}\
 count of subsets : 6
+
+
+## @Result
+![img.png](img.png)
--- src/SubsetSum.java
+++ src/SubsetSum.java
@@ -1,36 +1,41 @@
 import java.util.LinkedList;
 
+/**
+ * Subset Sum Problem: finding all subsets that sum to the target sum.
+ */
 public class SubsetSum {
     // instance variables
-
     int[] array = new int[]{1,2,3,4,5,6,7};
     int targetSum = 9;
     LinkedList<Integer> list = new LinkedList<>();
+    LinkedList<LinkedList<Integer>> subsets = new LinkedList<>();
+
     public SubsetSum() {
-        // TODO:
-        if(isSum(array, 0, targetSum)){
-            System.out.println("yes");
-        }else {
-            System.out.println("no");
-        }
+        findSubsets(array, 0, targetSum, list);
+        System.out.println("Subsets: "+subsets);
+        System.out.println("Count of subsets: "+subsets.size());
     }
 
-    public boolean isSum(int[] array, int index, int targetSum) {
-        // base-case: found the subset; targetSum = 0
+    private void findSubsets(int[] array, int index, int targetSum, LinkedList<Integer> currentList) {
+        // Base-Case: found the subset; targetSum = 0
         if (targetSum == 0){
-            return true;
+            subsets.add(new LinkedList<>(currentList)); // add the subset found.
+            return;
         }
-        // base-case2: when array is empty or targetSum < 0
-        if (array.length == 0 || targetSum < 0) {
-            return false;
+        // if index is out of bound or targetSub is negative, then return.
+        if (index >= array.length || targetSum < 0){
+            return;
         }
-        if ( index >= array.length) {
-            return false;
+        int currentValue = array[index];
+        if (array[index] > targetSum) {
+            return;
         }
 
+        currentList.add(currentValue);  // store data in temp linkedList
+        findSubsets(array, index+1, targetSum-currentValue, currentList);
 
-        return false;
-        // TODO: implement the logic to find subset sum.
+        currentList.removeLast();   // pop-out and skip current value, then just move to next index.
+        findSubsets(array, index+1, targetSum, currentList);
     }
 
     public static void main(String[] args) {
